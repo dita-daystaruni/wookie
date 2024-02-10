43e7c3050a0a849363100bf44047b8a1d9ff930a
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-02-10 17:01:58 +0300

    fix: added file type field to db

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class NotificationsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notifications
-        fields = ['contents', 'upload', 'validity', 'notification_link', 'style', 'sender']
+        fields = ['contents', 'upload', 'validity', 'notification_link', 'style', 'sender', 'file_type']
 
 class MessageSerializer(serializers.Serializer):
     message = serializers.CharField(max_length=100)
