7d8ac20dd839603ca813aaa0ae7494782ea4e7a2
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-04-01 15:49:25 +0300

    feat: added url for api that handles posting of course exam info

---
+++ timetable/urls.py
@@ -0,0 +1,8 @@
+"""Defines the urls to be used by the timetable app."""
+
+from django.urls import path
+from .views import ParseTimeTablesAPI
+
+urlpatterns = [
+    path("parse/", ParseTimeTablesAPI.as_view(), name="notifications"),
+]
