c0da4cd13017f112d468adf9e4514f8f1dea578e
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-04-01 15:48:00 +0300

    feat: added Model to store Course Exam Info

--- timetable/models.py
+++ timetable/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class CoursesExamInfo(models.Model):
+    """
+    Will hold info about course exam timetable info
+    """
+    course_code = models.CharField(max_length=15, default="",blank=False, unique=True)
+    day = models.CharField(max_length=50, default="",blank=False)
+    time = models.CharField(max_length=15, default="",blank=True)
+    venue = models.CharField(max_length=50, default="",blank=False)
+    campus = models.CharField(max_length=50, default="",blank=False)
+    coordinator = models.CharField(max_length=50, default="",blank=False)
+    hrs = models.CharField(max_length=5, default="",blank=False)
+    invigilator = models.CharField(max_length=50, default="",blank=False)
