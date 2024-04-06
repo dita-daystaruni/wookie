10154cf426cd4dd6d24774d51dee7d274e8518f1
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-04-06 13:39:48 +0300

    feat: added url to return exam course info

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,8 +1,9 @@
 """Defines the urls to be used by the timetable app."""
 
 from django.urls import path
-from .views import ParseTimeTablesAPI
+from .views import ParseTimeTablesAPI, ExamsCourseInfoAPI
 
 urlpatterns = [
     path("parse/", ParseTimeTablesAPI.as_view(), name="parser"),
+    path("exams/", ExamsCourseInfoAPI.as_view(), name="exams"),
 ]
